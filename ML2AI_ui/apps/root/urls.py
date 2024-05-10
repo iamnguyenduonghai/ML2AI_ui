748a4c94c5251e70e4ab9f8c17981bb4877b8c51
haind <haind@mtsolution.com.vn>
2024-05-11 00:06:28 +0700

    [master] update root

--- ML2AI_ui/apps/root/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('apps.root.home.urls'))
-]
--- ML2AI_ui/apps/supervised_learning/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('supervised_learning/', include('apps.supervised_learning.linear_reg.urls'))
-]
