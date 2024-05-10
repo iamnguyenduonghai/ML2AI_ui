11a9477856c6cd00e3f7464c7f7ada2238e8eaf3
haind <haind@mtsolution.com.vn>
2024-05-11 03:19:29 +0700

    [master] update root

--- apps/root/home/static/home/home.js
+++ apps/root/home/static/home/home.js
@@ -4,7 +4,7 @@ const $card = $('.card')
 const $collapse_menu_btn = $('#collapse-menu-btn')
 const $find = $('#find-algorithms')
 
-$card.on('click', function () {
+$tab_title.on('click', function (){
     $card.css({
         'background-color': '#F4F1EB',
         'color': '#2B77A4',
@@ -14,18 +14,16 @@ $card.on('click', function () {
     })
     $card.removeClass('current-card')
     $card.find('.pin-icon').prop('hidden', true)
-    $(this).css({
+    $(this).find('.card').css({
         'background-color': '#2B77A4',
         'color': '#F4F1EB',
         'text-align': 'right',
         'transform': 'translateX(30px)',
         'transition': 'transform 0.1s ease-in-out'
     })
-    $(this).addClass('current-card')
-    $(this).find('.pin-icon').prop('hidden', false)
-})
+    $(this).find('.card').addClass('current-card')
+    $(this).find('.card').find('.pin-icon').prop('hidden', false)
 
-$tab_title.on('click', function (){
     $tab_content.prop('hidden', true)
     let tab_id = `#${$(this).attr('id')}-content`
     $(tab_id).prop('hidden', false)
